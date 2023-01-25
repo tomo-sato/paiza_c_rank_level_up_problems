3067e776fdde0f9d8ab4561645559504ff03ec06
Tomomichi Sato <tsato@dcworks.jp>
2023-01-25 14:45:17 +0900

    EngineersGate paiza課題実施。

--- src/c_rank_simulation_boss/Main.java
+++ src/c_rank_simulation_boss/Main.java
@@ -9,13 +9,76 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int hp = sc.nextInt();
 
-		int monsterAttackCount = 0;
+		int paizaAttack1 = 0;
+		int paizaAttack2 = 0;
+		int monsterAttack1 = 0;
+		int monsterAttack2 = 0;
 
-		int paizaAttack = 1;
-		int monsterAttack = 1;
+		int i = 0;
+		while (hp > 0) {
+			i++;
 
-		for (int i = hp; i < 0;) {
+			// パイザ君の攻撃
+			int tmpPaizaAttack = getPaizaAttackDamage(i, monsterAttack2, monsterAttack1);
+
+			// モンスターの攻撃
+			int tmpMonsterAttack = getMonsterAttackDamage(i, paizaAttack2, paizaAttack1);
+			hp = hp - tmpMonsterAttack;
+
+			paizaAttack1 = paizaAttack2;
+			paizaAttack2 = tmpPaizaAttack;
+
+			monsterAttack1 = monsterAttack2;
+			monsterAttack2 = tmpMonsterAttack;
 
 		}
+
+		System.out.println(i);
+	}
+
+	/**
+	 * パイザ君が与えるダメージ
+	 *
+	 * @param count n回目の攻撃を表すカウント。
+	 * @param damage1 受けたダメージ1
+	 * @param damage2 受けたダメージ2
+	 * @return モンスターに与えるダメージ
+	 */
+	private static int getPaizaAttackDamage(int count, int damage1, int damage2) {
+		int retDamage = 0;
+		switch (count) {
+			case 1, 2:
+				retDamage = 1;
+				break;
+
+			default:
+				retDamage = damage1 + damage2;
+				break;
+		}
+
+		return retDamage;
+	}
+
+	/**
+	 * モンスターが与えるダメージ
+	 *
+	 * @param count n回目の攻撃を表すカウント。
+	 * @param damage1 受けたダメージ1
+	 * @param damage2 受けたダメージ2
+	 * @return パイザ君に与えるダメージ
+	 */
+	private static int getMonsterAttackDamage(int count, int damage1, int damage2) {
+		int retDamage = 0;
+		switch (count) {
+			case 1, 2:
+				retDamage = 1;
+				break;
+
+			default:
+				retDamage = (damage1 * 2) + damage2;
+				break;
+		}
+
+		return retDamage;
 	}
 }
